f8958676578be8d6f3f284ad617b3aa9615f18dc
Yaron Veg <Yaronveg1@gmail.com>
2021-10-21 12:08:07 +0300

    list improve 1st stage

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,8 +2,19 @@ import Product from "../Product/Product";
 import "../Product/Product.css";
 
 function Products() {
+  const itemNum = 10;
+  const listItems = [];
+  for (let i = 0; i < itemNum; i++) {
+    listItems.push(
+      <li>
+        <Product />
+      </li>
+    );
+  }
+
   return (
     <section className="products">
+      {/* <ul>{listItems}</ul> */}
       <Product />
       <Product />
       <Product />
