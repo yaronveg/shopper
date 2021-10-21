8a81ec7da6c53d04ab58466ae17c4010df0fd4d3
Yaron Veg <yaronveg1@gmail.com>
2021-10-21 10:32:41 +0300

    1st commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,18 @@
+function Product() {
+  return (
+    <div className="product-card">
+      <div className="product-image">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
+          alt="Img"
+        />
+      </div>
+      <div className="product-info">
+        <h5>Winter Jacket</h5>
+        <h6>$99.99</h6>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
---
+++ src/components/Products/Products.js
@@ -0,0 +1,21 @@
+import Product from "../Product/Product";
+import "../Product/Product.css";
+
+function Products() {
+  return (
+    <section className="products">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </section>
+  );
+}
+
+export default Products;
