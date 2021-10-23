ae9a06cb46018101c62dc53c74beb61221677ed6
Yaron Veg <Yaronveg1@gmail.com>
2021-10-23 20:20:14 +0300

    improve product array creation

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,14 +3,11 @@ import Product from "../Product/Product";
 
 function Products() {
   const itemNum = 10;
-  const listItems = [];
-  for (let i = 0; i < itemNum; i++) {
-    listItems.push(
-      <>
-        <Product />
-      </>
-    );
-  }
+  const listItems = new Array(itemNum).fill(
+    <>
+      <Product />
+    </>
+  );
 
   return <section className="products">{listItems}</section>;
 }
