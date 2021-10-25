85ff2fa41fecc685f123ca4538cd3dc8d32a037e
Yaron Veg <yaronveg1@gmail.com>
2021-10-25 18:41:09 +0300

    data from moc-server

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,17 +1,14 @@
 import "./Product.css";
 
-function Product() {
+function Product({ id, title, price, image }) {
   return (
-    <div className="product-card">
+    <div className="product-card" id={id}>
       <div className="product-image">
-        <img
-          src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369"
-          alt="Img"
-        />
+        <img src={image} alt="Img" />
       </div>
       <div className="product-info">
-        <h5>Winter Jacket</h5>
-        <h6>$99.99</h6>
+        <h5>{title}</h5>
+        <h6>${price}</h6>
       </div>
     </div>
   );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,12 @@
 import "./Products.css";
 import Product from "../Product/Product";
 
-function Products() {
-  const itemNum = 6;
-  const listItems = new Array(itemNum).fill(<Product />);
-  let i = -1;
-  const newList = listItems.map((prod) => {
-    i++;
-    return <Product key={i} />;
-  });
+function Products(data) {
+  console.log(data.data);
 
+  const newList = data.data.map(({ id, title, price, image }) => (
+    <Product id={id} title={title} price={price} image={image} />
+  ));
   return <section className="products">{newList}</section>;
 }
 
