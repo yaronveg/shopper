3cc4767e4efe6b291eee3925b0976270bae8db88
Yaron Veg <yaronveg1@gmail.com>
2021-10-25 18:18:08 +0300

    key error fix

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,13 +3,14 @@ import Product from "../Product/Product";
 
 function Products() {
   const itemNum = 6;
-  const listItems = new Array(itemNum).fill(
-    <>
-      <Product />
-    </>
-  );
+  const listItems = new Array(itemNum).fill(<Product />);
+  let i = -1;
+  const newList = listItems.map((prod) => {
+    i++;
+    return <Product key={i} />;
+  });
 
-  return <section className="products">{listItems}</section>;
+  return <section className="products">{newList}</section>;
 }
 
 export default Products;
