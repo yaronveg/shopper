bdaa2116086fc7ee514d203c8681cc074ddb202e
Yaron Veg <yaronveg1@gmail.com>
2021-10-26 11:04:25 +0300

    passing data end of lesson

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,8 +1,8 @@
 import "./Products.css";
 import Product from "../Product/Product";
 
-function Products(products) {
-  const productList = products.products.map(({ id, title, price, image }) => (
+function Products({ products }) {
+  const productList = products.map(({ id, title, price, image }) => (
     <Product key={id} id={id} title={title} price={price} image={image} />
   ));
 
