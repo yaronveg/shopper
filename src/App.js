c773f6b8dedfe3043a38db7c1d2797a460acfdd0
Yaron Veg <yaronveg1@gmail.com>
2021-11-10 19:33:48 +0200

    price range

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { useState, useEffect } from "react";
+import * as React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
 import Cart from "./components/Cart/Cart";
 import CartContext from "./CartContext";
+// import RangeSlider from "./components/Slider/Slider";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+
+function pricetext(price) {
+  return `${price}$`;
+}
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -11,7 +19,6 @@ function App() {
   const handleCatChange = (categoryChange) => {
     setCategory(categoryChange);
   };
-  // const [cart, setCart] = useState({ cartShow: false, cartProducts: [] });
   const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch("https://fakestoreapi.com/products").then((response) =>
@@ -25,6 +32,15 @@ function App() {
       .map((p) => p.category)
       .filter((value, index, array) => array.indexOf(value) === index);
   }
+
+  const maxPrice = Math.max(...products.map((product) => product.price));
+  const minPrice = Math.min(...products.map((product) => product.price));
+
+  const [priceRange, setPriceRange] = React.useState([0, 1500]);
+
+  const handleChange = (event, newPriceRange) => {
+    setPriceRange(newPriceRange);
+  };
   return (
     <div className="App">
       <CartContext.Provider value={{ cart, setCart }}>
@@ -37,8 +53,23 @@ function App() {
             setCartShow={setCartShow}
           />
         )}
+        <Box sx={{ width: 300 }}>
+          <Slider
+            getAriaLabel={() => "Price Range"}
+            value={priceRange}
+            onChange={handleChange}
+            valueLabelDisplay="auto"
+            getAriaValueText={pricetext}
+            min={minPrice}
+            max={maxPrice}
+          />
+        </Box>
         {products && (
-          <Products products={products} currentCategory={category} />
+          <Products
+            products={products}
+            currentCategory={category}
+            priceRange={priceRange}
+          />
         )}
       </CartContext.Provider>
     </div>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,14 +1,21 @@
 import "./Products.css";
 import Product from "../Product/Product";
 
-function Products({ products, currentCategory }) {
+function Products({ products, currentCategory, priceRange }) {
+  console.log(priceRange);
+  const productsRanged = products.filter(
+    (product) =>
+      product.price >= priceRange[0] && product.price <= priceRange[1]
+  );
+
   let filtered = [];
+
   if (currentCategory !== "All") {
-    filtered = products.filter(
+    filtered = productsRanged.filter(
       (product) => product.category === currentCategory
     );
   } else {
-    filtered = products;
+    filtered = productsRanged;
   }
   const productList = filtered.map(({ id, title, price, image }) => (
     <Product key={id} id={id} title={title} price={price} image={image} />
